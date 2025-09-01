16b8803eb68f3732b90d7a3dc591665ee5a206c0
Techno <elijahprosperie@gmail.com>
2025-09-01 12:01:12 -0700

    Variable introduction complete.

---
+++ Daily Lessons/Variables/index.js
@@ -0,0 +1,7 @@
+let myName = "Techno Dev"
+let age = 25
+let isAStudent = true;
+
+document.getElementById("name").textContent = myName;
+document.getElementById("age").textContent = `I am currently ${age} years old.`
+document.getElementById("school").textContent = `Enrolled as a student: ${isAStudent}`
