81dfb9b20058a0573130ea4c4ec92cf84d7c3c60
Techno <technov@Elijahs-MacBook-Pro.local>
2025-09-06 05:14:15 -0700

    Ternary operator complete

---
+++ Daily Lessons/Ternary Operator/index.js
@@ -0,0 +1,7 @@
+//The ternary operator is a shorthanded way to write and use the if else statements.
+
+const age = 19
+
+const legalAge = age >= 18 ? "Welcome to adult life!" : "Sorry, you still have a few years to go."
+console.log(legalAge);
+
